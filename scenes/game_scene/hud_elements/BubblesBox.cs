8a2d909edb46223f78a715c7ea0a814c7ce9902d
Nathan Coleman <149843063+nathan-coleman@users.noreply.github.com>
2025-07-20 08:58:26 -0700

    Set up demo bubbles box

--- scenes/game_scene/hud_elements/BubblesBox.cs
+++ scenes/game_scene/hud_elements/BubblesBox.cs
@@ -1,7 +1,42 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace IndieGameDev.Game.UI;
 
 public partial class BubblesBox : Control
 {
+    [Export] private PackedScene bubbleBarItemPrefab;
+
+    private static readonly Dictionary<string, int> demoBubbleValues = new()
+    {
+        { "Story", 10 },
+        { "Backend", 20 },
+        { "Gameplay", 40 },
+        { "Sound", 80 },
+        { "Progression", 50 },
+        { "Hype", 70 },
+        { "Polish", 30 }
+    };
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        var bubbleBarContainer = GetNode<Container>("%BubbleBarContainer");
+
+        foreach (Node childNode in bubbleBarContainer.GetChildren())
+        {
+            childNode.QueueFree();
+        }
+
+        foreach (var bubbleValue in demoBubbleValues)
+        {
+            var newBubbleBarItem = bubbleBarItemPrefab.Instantiate() as BubbleBarItem;
+
+            newBubbleBarItem.BubbleName = bubbleValue.Key;
+            newBubbleBarItem.BubbleAmount = bubbleValue.Value;
+
+            bubbleBarContainer.AddChild(newBubbleBarItem);
+        }
+    }
 }
